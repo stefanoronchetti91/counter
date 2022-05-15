bd703828ce17706b8083512e5ea0bb9ccb1c775c
ciacco91 <92170060+ciacco91@users.noreply.github.com>
2022-05-15 17:27:10 +0200

    v1.1 reset button add

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const increaseButton = document.querySelector('#increase');
 const decreaseButton = document.querySelector('#decrease');
 const actualCounter = document.querySelector('#actual-value');
+const resetButton = document.querySelector('#reset');
 
 let count = 0;
 
@@ -8,6 +9,7 @@ updateCounter();
 
 increaseButton.addEventListener("click", increase);
 decreaseButton.addEventListener("click", decrease);
+resetButton.addEventListener("click", reset)
 
 
 function increase () {
@@ -17,6 +19,11 @@ function increase () {
 
 function decrease () {
     count --;
+    updateCounter();
+}
+
+function reset () {
+    count = 0;
     updateCounter();
 }
 
